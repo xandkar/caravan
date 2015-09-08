1788bfacf77f4971005199af6d29c6178369428c
Siraaj Khandkar <siraaj@khandkar.net>
2015-09-08 04:01:19 -0400

    Match on concrete state value in simple example.
    To remove some cognitive load of wondering whether "state" matters.

--- examples/hello/hello.ml
+++ examples/hello/hello.ml
@@ -5,19 +5,19 @@ module Log  = Caravan.Log
 module Test = Caravan.Test
 
 let test_hello_pass =
-  let case state ~log =
+  let case () ~log =
     Log.info  log "Info level is always reported.";
     Log.debug log "Asserting 2 + 2 = 4";
     assert (2 + 2 = 4);
-    return state
+    return ()
   in
   {Test.id = "test_hello_pass"; case; children = []}
 
 let test_hello_fail =
-  let case state ~log =
+  let case () ~log =
     Log.debug log "Asserting 2 + 2 = 5";
     assert (2 + 2 = 5);
-    return state
+    return ()
   in
   {Test.id = "test_hello_fail"; case; children = []}
 
